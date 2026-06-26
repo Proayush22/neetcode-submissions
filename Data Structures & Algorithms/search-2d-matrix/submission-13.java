a61fd7415fbb83d7710b207334989d0c57c696ab
Ayush Sinha <135677365+Proayush22@users.noreply.github.com>
2026-06-26 02:21:30 -0400

    Add: search-2d-matrix - submission-13

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-13.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int left = 0;
+        int right = matrix.length - 1;
+        int mrow = 0;
+
+        while(left < right){
+            mrow = left + (right - left) / 2;
+            if(target < matrix[mrow][0]){
+                right = mrow - 1;
+            } else if(target > matrix[mrow][matrix[mrow].length - 1]){
+                left = mrow + 1;
+            }
+            else {
+                left = mrow;
+                right = mrow;
+            }
+        }
+        
+        mrow = left;
+        left = 0;
+        right = matrix[mrow].length - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(target < matrix[mrow][mid]){
+                right = mid - 1;
+
+            } else if (target > matrix[mrow][mid]) {
+                left = mid + 1;
+
+            } else if (target == matrix[mrow][mid]) {
+                return true;
+
+            }
+        }
+        return false;
+
+
+    }
+}
